cacbba6c43cb04c0273d21a02560a914422056e9
ilnarkz <ilnar-16_04@mail.ru>
2020-07-27 20:54:03 +0300

    gcd game

--- brain_games/games/brain_gcd_game.py
+++ brain_games/games/brain_gcd_game.py
@@ -11,7 +11,7 @@ def main():
     while counter < 3:
         num1 = randint(1, 100)
         num2 = randint(1, 100)
-        
+
         def pair_of_numbers(num1, num2):
             while num1 != num2:
                 if num1 > num2:
